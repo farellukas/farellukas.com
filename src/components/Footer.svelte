be388a765daf855bb9cbc9afa642606869c04059
Farel Lukas <farellukas@gmail.com>
2025-02-24 10:27:53 -0700

    init astro project

--- src/components/Footer.svelte
+++
@@ -1,3 +0,0 @@
-<footer class="flex justify-center my-4">
-	<p class="text-xs text-[#628E90]">made by Farel Lukas</p>
-</footer>
--- src/routes/+error.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import { page } from '$app/stores';
-</script>
-
-<div class="text-center">
-	<h1>Error {$page.status}</h1>
-	<h2>{$page.error?.message}</h2>
-</div>
